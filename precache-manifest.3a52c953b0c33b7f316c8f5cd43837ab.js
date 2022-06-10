69c42b7c15efc8baeae56ce6c922c237c1f05dec
Nikolas-Bars <kasp2409@mail.ru>
2022-06-10 21:07:53 +0300

    Updates

---
+++ precache-manifest.3a52c953b0c33b7f316c8f5cd43837ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea32606b15553f2683dc533c01226ab",
+    "url": "/FridayProject/index.html"
+  },
+  {
+    "revision": "a2da47dff0c86a0939ad",
+    "url": "/FridayProject/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "7349192dde812deec85b",
+    "url": "/FridayProject/static/js/2.9c35b9c4.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/FridayProject/static/js/2.9c35b9c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2da47dff0c86a0939ad",
+    "url": "/FridayProject/static/js/main.1085a00b.chunk.js"
+  },
+  {
+    "revision": "c4a4102098b83929562b",
+    "url": "/FridayProject/static/js/runtime-main.8a92cd8d.js"
+  }
+]);
